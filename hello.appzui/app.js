5c0869740bd399791bed2ba8e5be5e6206f36ca1
snivas <agsnivas.com>
2015-02-15 21:25:42 +0100

    all commit

--- hello.appzui/app.js
+++ hello.appzui/app.js
@@ -1,59 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+var express = require('express')
+, socketio = require('socket.io');
+var app     = express()
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+app.use(express.static(__dirname + '/public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var databaseUrl = "mongodb://user1:user1@ds039321.mongolab.com:39321/appzui"; // "username:password@example.com/mydb"
+var collections = ["state", "history"]
+var db = require("mongojs").connect(databaseUrl, collections);
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 
 app.use('/', routes);
-app.use('/users', users);
+app.set('views', './views');
+app.set('view engine', 'jade');
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+var server = app.listen(3000, function () {
+var host = server.address().address
+var port = server.address().port
+var newmsg = [];
 
-/// error handlers
+  console.log('Example app listening at http://%s:%s', host, port)
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+})
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+socketio.listen(server).on('connection', function (socket) {
+    socket.on('setstatus', function (status) {
+        console.log('Message Received: ', status);
+        socket.broadcast.emit('setstatus', status);       
+        newmsg = JSON.parse(JSON.stringify(status));
+        db.state.update({_id: newmsg.lightclass}, newmsg,{ upsert: true });
+        db.history.save({timestp: new Date().toString(), light: newmsg.lightclass, state: newmsg.newstatus }, function(err, saved) {
+      	  if( err || !saved ) console.log("not saved");
+      	  else console.log("saved");
+        });        
     });
 });
-
-
-module.exports = app;
--- hello.appzui/routes/index.js
+++ hello.appzui/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET  listing. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	  res.render('index', {
+	    title: 'Hello AppZui'
+	  });
 });
+
+router.get('/states/:lightid', function(req, res) {
+	var db = req.db;
+	var lightid = req.params.lightid;
+	console.log("Get " + lightid);
+	db.state.find({_id : lightid}).toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+router.get('/history', function(req, res) {
+	var db = req.db;
+	db.history.find({},{"limit": 10}).sort( { timestp: -1 } ).toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
 
 module.exports = router;
